f6339362da1b9c867549ce083d6342f9524322b2
Sean Griffin <sean@seantheprogrammer.com>
2019-01-22 14:52:26 -0700

    Scaffold the bot
    This is just some basic scaffolding to set up the dependencies, and get
    the main login/message handler out of the way. Right now the auth is
    just "are you a user defined in an environment variable", and the
    only command is responding to ping with pong

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,40 @@
-console.log("Hello, World");
+import * as dotenv from "dotenv";
+import * as Discord from "discord.js";
+
+dotenv.config();
+const client = new Discord.Client();
+const authorizedUsers = process.env.AUTHORIZED_USERS.split(",");
+
+let atMention: string;
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  atMention = `<@${client.user.id}>`;
+});
+
+client.on("message", msg => {
+  if (!isDmOrMentionInOpsChannel(msg)) {
+    return;
+  }
+
+  const [command, ...args] = msg.content.replace(`${atMention} `, "").split(" ");
+
+  if (!userCanExecuteCommand(msg.author, command, args)) {
+    return;
+  }
+
+  if (command == "ping") {
+    msg.channel.send("pong");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+function isDmOrMentionInOpsChannel(msg: Discord.Message): boolean {
+  return msg.channel.type === "dm" ||
+    (<Discord.TextChannel> msg.channel).name === "crates-io" && msg.content.startsWith(atMention);
+}
+
+function userCanExecuteCommand(user: Discord.User, command: string, args: string[]): boolean {
+  return authorizedUsers.includes(user.id.toString());
+}
