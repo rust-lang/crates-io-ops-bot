49f8d63bf99ff4c94f66aaf8ad8b368347d19708
Nell Shamrell <nellshamrell@gmail.com>
2020-02-13 16:22:30 -0800

    removes restriction from ping command
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -10,14 +10,7 @@ fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     println!("Message {:?}", author_name);
     println!("running pong command");
 
-    if is_authorized(author_name.to_string()) {
-        msg.reply(ctx, "Pong!")?;
-    } else {
-        msg.reply(
-            ctx,
-            format!("{} is not authorized to run this command", msg.author.name),
-        )?;
-    }
+    msg.reply(ctx, "Pong!")?;
 
     Ok(())
 }
