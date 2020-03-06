e6b2947c286f02943fa30b2e3797c94a9c4fafb0
Nell Shamrell <nellshamrell@gmail.com>
2020-03-06 10:48:13 -0800

    refactor parsing users string to the config module so we only have to do it once for the config
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/authorizations/users.rs
+++ src/authorizations/users.rs
@@ -1,20 +1,12 @@
 use crate::config::config::Config;
 
-fn authorized_users(config: &Config) -> Vec<String> {
-    let split_string = config.authorized_users.split(',');
-    let auth_users: Vec<String> = split_string.map(String::from).collect();
-
-    auth_users
-}
-
 pub fn is_authorized(id: &str, config: &Config) -> bool {
-    authorized_users(config).iter().any(|i| i == id)
+    config.authorized_users.iter().any(|i| i == id)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::env;
 
     fn test_config() -> Config {
         Config::new(
@@ -22,23 +14,6 @@ mod tests {
             String::from("123,456"),
             String::from("456"),
         )
-    }
-
-    #[test]
-    fn list_authorized_users() {
-        let config = test_config();
-        let result = authorized_users(&config);
-        assert!(
-            result.contains(&String::from("123")),
-            "Result does not contain the expected name. Result was {:?}",
-            result
-        );
-
-        assert!(
-            result.contains(&String::from("456")),
-            "Result does not contain the expected name. Result was {:?}",
-            result
-        );
     }
 
     #[test]
--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 pub mod config {
     use serenity::prelude::TypeMapKey;
+    use std::collections::HashSet;
 
     #[derive(Debug)]
     pub struct Config {
         pub discord_token: String,
-        pub authorized_users: String,
+        pub authorized_users: HashSet<String>,
         pub heroku_api_key: String,
     }
 
@@ -16,7 +17,7 @@ pub mod config {
         ) -> Config {
             Config {
                 discord_token,
-                authorized_users,
+                authorized_users: authorized_users_set(authorized_users),
                 heroku_api_key,
             }
         }
@@ -24,5 +25,32 @@ pub mod config {
 
     impl TypeMapKey for Config {
         type Value = Config;
+    }
+
+    fn authorized_users_set(users: String) -> HashSet<String> {
+        let mut users_set = HashSet::new();
+
+        let split_string = users.split(',');
+
+        for string in split_string {
+            users_set.insert(String::from(string));
+        }
+
+        users_set
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn create_authorized_users_hashset() {
+            let test_string = String::from("123,456,789");
+            let users_set = authorized_users_set(test_string);
+
+            assert!(users_set.contains("123"));
+            assert!(users_set.contains("456"));
+            assert!(users_set.contains("789"));
+        }
     }
 }
