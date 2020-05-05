d50cccb89601d8b73b1d82ec35bd3406b936bf46
Nell Shamrell <nellshamrell@gmail.com>
2020-05-05 16:14:05 -0700

    require two args for multiply command
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -3,6 +3,7 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
+#[num_args(2)]
 #[description = "Multiplies two numbers"]
 #[example = "~multiply 2 5"]
 pub fn multiply(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
