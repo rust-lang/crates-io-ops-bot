1cd744e5ae06be1f388a3d8e3316b98320aa76ba
Nell Shamrell <nellshamrell@gmail.com>
2020-05-05 16:05:58 -0700

    adds in help text for ping command
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -3,6 +3,9 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
+#[description = "Returns a pong message"]
+#[usage = "Play ping pong"]
+#[example = "~ping"]
 fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!")?;
 
