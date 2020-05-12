7767d7022c4a0e558a03c977dc20d35a2370db8b
Nell Shamrell <nellshamrell@gmail.com>
2020-05-11 17:05:44 -0700

    wip
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- terraform_example/main.tf
+++ terraform_example/main.tf
@@ -1,5 +1,35 @@
 provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
+    region = var.aws_region
+    profile = var.aws_profile
+}
+
+resource "aws_ecs_service" "crates-io-ops-bot" {
+    name = "crates-io-ops-bot2"
+    task_definition = aws_ecs_task_definition.crates-io-ops-bot.arn
+    launch_type = "FARGATE"
+}
+
+resource "aws_cloudwatch_log_group" "crates-io-ops-bot" {
+    name = "/ecs/crates-io-ops-bot"
+}
+
+data "template_file" "crates-io-ops-bot" {
+  template = file("./templates/crates-io-ops-bot.json.tpl")
+
+  vars = {
+    app_image      = var.app_image
+    app_port       = var.app_port
+    fargate_cpu    = var.fargate_cpu
+    fargate_memory = var.fargate_memory
+    aws_region     = var.aws_region
+  }
+}
+
+resource "aws_ecs_task_definition" "crates-io-ops-bot" {
+    family = "crates-io-ops-bot"
+    container_definitions = data.template_file.crates-io-ops-bot.rendered 
+    cpu = 256
+    memory = 512
+    requires_compatibilities = ["FARGATE"]
+    network_mode = "awsvpc"
 }
--- terraform_example/variables.tf
+++ terraform_example/variables.tf
@@ -1,15 +1,50 @@
-variable "region" {
-    type = string
+variable "aws_region" {
     default = "us-west-2"
     description = "aws region to deploy to"
 }
 
-variable "access_key" {
-    type = string
-    description = "AWS Access Key ID"
+variable "aws_profile" {}
+
+variable "availability_zone" {
+    default = "us-west-1b"
 }
 
-variable "secret_key" {
-    type = string
-    description = "AWS Secret Key"
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default = "cratesioopsbotEcsTaskExecutionRole"
+}
+
+variable "ecs_auto_scale_role_name" {
+  description = "ECS auto scale role Name"
+  default = "myEcsAutoScaleRole"
+}
+
+variable "app_image" {
+    description = "Docker image to run in the ECS cluster"
+    default = "https://hub.docker.com/repository/docker/nellshamrell/crates-io-ops-bot"
+}
+
+variable "app_port" {
+    description = "Port exposed by the docker image to redirect traffic to"
+    default = 8888
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)" 
+   default = "256"
+}
+
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
 }
