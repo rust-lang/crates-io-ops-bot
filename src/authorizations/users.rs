1152b193ff277e3cb3917f8183cb988176f23117
Nell Shamrell <nellshamrell@gmail.com>
2020-05-20 15:34:34 -0700

    corrects formatting
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/authorizations/users.rs
+++ src/authorizations/users.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 // Checks for permissions in https://github.com/rust-lang/team/
 #[derive(Debug, Deserialize, Clone)]
 struct TeamResponse {
-    discord_ids: Vec<usize> 
+    discord_ids: Vec<usize>,
 }
 
 #[derive(Debug)]
@@ -17,7 +17,7 @@ impl TeamClient {
         let team_client = reqwest_client::new();
 
         TeamClient {
-            client: team_client
+            client: team_client,
         }
     }
 }
@@ -28,7 +28,9 @@ pub fn is_authorized(id: &str) -> bool {
 }
 
 fn discord_id_in_list(id: &str, team_response: TeamResponse) -> bool {
-    team_response.discord_ids.contains(&id.parse::<usize>().unwrap())
+    team_response
+        .discord_ids
+        .contains(&id.parse::<usize>().unwrap())
 }
 
 fn team_info() -> TeamResponse {
@@ -42,9 +44,7 @@ fn get_team_info() -> Result<TeamResponse, reqwest::Error> {
         &String::from("https://team-api.infra.rust-lang.org/v1/permissions/crates_io_ops_bot.staging_crates_io.json")
     );
 
-    let team_response = team_request
-        .send()
-        .and_then(|res| res.error_for_status())?;
+    let team_response = team_request.send().and_then(|res| res.error_for_status())?;
 
     let response_text = team_response.text().unwrap();
 
@@ -62,7 +62,7 @@ mod tests {
         let fake_id_vec = vec![fake_id];
 
         TeamResponse {
-            discord_ids: fake_id_vec
+            discord_ids: fake_id_vec,
         }
     }
 
